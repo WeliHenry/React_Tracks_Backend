dc4f8427a5e4c083640f092388fba8dcbefd7394
henry weli <henrysnoops@gmail.com>
2022-04-18 02:26:24 +0100

    added schema.py file to the root project

--- React_Tracks_Backend/settings.py
+++ React_Tracks_Backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -27,24 +28,21 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # third party apps
-
     'graphene_django',
-
-    #     Created apps
     'tracks.apps.TracksConfig',
 
 
+
 ]
+
 
 GRAPHENE = {
     'SCHEMA': 'React_Tracks_Backend.schema.schema'
